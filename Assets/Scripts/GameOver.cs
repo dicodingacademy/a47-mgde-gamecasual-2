8d8ddd7a58d36ea941b17bbfae47382a9cabcf7b
Buchori Rafsanjani <choryforever@yahoo.co.id>
2018-11-15 23:37:45 +0700

    Add files via upload
    Game Casual 2

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    float timer = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 2)
+        {
+            Data.score = 0;
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
